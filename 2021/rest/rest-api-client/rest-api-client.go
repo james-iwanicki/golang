b14a9bbc223e694c64e0ee9d63df3c0042600e74
root <root@ip-172-31-26-34.us-west-2.compute.internal>
2021-08-07 14:54:56 -0400

    Add Insert/Delete functionality

--- 2021/rest/rest-api-client/rest-api-client.go
+++ 2021/rest/rest-api-client/rest-api-client.go
@@ -9,12 +9,13 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"encoding/json"
 )
 
 type User struct {
-	ID int `json: "ID"`
-	Name string `json: "Name"`
-	Age int `json: "Age"`
+        ID int  `json: "ID"`
+        Name string `json: "Name"`
+        Age int `json: "Age"`
 }
 
 var (
@@ -87,10 +88,121 @@ func get_user_secure_func(c *gin.Context) {
 	c.Writer.Write([]byte(fmt.Sprintf("%s\n", string(data))))
 }
 
+func add_user_secure_func(c *gin.Context) {
+	var user User
+	c.Bind(&user)
+	fmt.Printf("Name:%s Age:%d\n", user.Name, user.Age)
+
+	jdata, jerr := json.Marshal(user)
+	if jerr != nil {
+		c.AbortWithError(http.StatusBadRequest, jerr)
+		return
+	}
+
+	parse, parse_err := url.Parse("https://rest-api-server-service.jeevan-namespace.svc.cluster.local/apis/v1/add_user")
+	if parse_err != nil {
+		c.AbortWithError(http.StatusBadRequest, parse_err)
+		return
+	}
+
+	req, req_err := http.NewRequest(http.MethodPost, parse.String(), bytes.NewBuffer(jdata))
+	if req_err != nil {
+		c.AbortWithError(http.StatusBadRequest, req_err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/json")
+
+	ca_cert, ca_cert_err := ioutil.ReadFile("/etc/secret-volume/ca.crt")
+	if ca_cert_err != nil {
+		c.AbortWithError(http.StatusBadRequest, ca_cert_err)
+		return
+	}
+	certpool := x509.NewCertPool()
+	certpool.AppendCertsFromPEM(ca_cert)
+
+
+
+	kp, kp_err := tls.LoadX509KeyPair("/etc/secret-volume/tls.crt", "/etc/secret-volume/tls.key")
+	if kp_err != nil {
+		c.AbortWithError(http.StatusBadRequest, kp_err)
+		return
+	}
+
+	client := &http.Client{
+		Transport : &http.Transport {
+			TLSClientConfig : &tls.Config {
+				RootCAs: certpool,
+				Certificates: []tls.Certificate { kp },
+			},
+		},
+	}
+	resp, resp_err := client.Do(req)
+        if resp_err != nil {
+                c.AbortWithError(http.StatusBadRequest, resp_err)
+                return
+        }
+        defer resp.Body.Close()
+	
+}
+
+func delete_user_secure_func(c *gin.Context) {
+	Name := c.Query("Name")
+
+	parse, parse_err := url.Parse("https://rest-api-server-service.jeevan-namespace.svc.cluster.local/apis/v1/delete_user")
+	if parse_err != nil {
+		c.AbortWithError(http.StatusBadRequest, parse_err)
+		return
+	}
+	values := url.Values{}
+	values.Add("Name", Name)
+	parse.RawQuery = values.Encode()
+
+	var jdata []byte
+	req, req_err := http.NewRequest(http.MethodDelete, parse.String(), bytes.NewBuffer(jdata))
+	if req_err != nil {
+		c.AbortWithError(http.StatusBadRequest, req_err)
+		return
+	}
+
+	ca_cert, ca_cert_err := ioutil.ReadFile("/etc/secret-volume/ca.crt")
+	if ca_cert_err != nil {
+		c.AbortWithError(http.StatusBadRequest, ca_cert_err)
+		return
+	}
+
+	certpool := x509.NewCertPool()
+	certpool.AppendCertsFromPEM(ca_cert)
+
+	kp, kp_err := tls.LoadX509KeyPair("/etc/secret-volume/tls.crt", "/etc/secret-volume/tls.key")
+	if kp_err != nil {
+		c.AbortWithError(http.StatusBadRequest, kp_err)
+		return
+	}
+
+	client := &http.Client {
+		Transport: &http.Transport {
+			TLSClientConfig: &tls.Config {
+				RootCAs: certpool,
+				Certificates: []tls.Certificate { kp },
+			},
+		},
+	}
+
+	resp, resp_err := client.Do(req)
+	if resp_err != nil {
+		c.AbortWithError(http.StatusBadRequest, resp_err)
+		return
+	}
+	defer resp.Body.Close()
+
+}
+
 func main() {
 	v := router.Group("/apis/v1")
 	{
 		v.GET("/get_user", get_user_secure_func)
+		v.POST("/add_user", add_user_secure_func)
+		v.DELETE("/delete_user", delete_user_secure_func)
 	}
 	router.Run(":80")
 }
--- 2021/rest/rest-api-server/rest-api-server.go
+++ 2021/rest/rest-api-server/rest-api-server.go
@@ -60,10 +60,73 @@ func get_user_secure_func(c *gin.Context) {
 
 }
 
+func add_user_secure_func(c *gin.Context) {
+	var user User
+	c.Bind(&user)
+	fmt.Printf("jejej %s %d\n", user.Name, user.Age)
+
+	db, db_err := sql.Open("mysql", "root:password@tcp(mysql:3306)/test")
+	if db_err != nil {
+		c.AbortWithError(http.StatusBadRequest, db_err)
+		return
+	}
+	defer db.Close()
+
+	stmt, stmt_err := db.Prepare("insert users set Name=?, Age=?")
+	if stmt_err != nil {
+		c.AbortWithError(http.StatusBadRequest, stmt_err)
+		return
+	}
+	
+	rows, rows_err := stmt.Query(user.Name, user.Age)
+	if rows_err != nil {
+		c.AbortWithError(http.StatusBadRequest, rows_err)
+		return
+	}
+
+	err := rows.Err()
+        if err != nil {
+                c.AbortWithError(http.StatusBadRequest, err)
+                return
+        }
+}
+
+func delete_user_secure_func(c *gin.Context) {
+	Name := c.Query("Name")
+
+	db, db_err := sql.Open("mysql", "root:password@tcp(mysql:3306)/test")
+        if db_err != nil {
+                c.AbortWithError(http.StatusBadRequest, db_err)
+                return
+        }
+        defer db.Close()
+
+
+	stmt, stmt_err := db.Prepare("delete from users where Name=?")
+        if stmt_err != nil {
+                c.AbortWithError(http.StatusBadRequest, stmt_err)
+                return
+        }
+
+	rows, rows_err := stmt.Query(Name)
+	if rows_err != nil {
+                c.AbortWithError(http.StatusBadRequest, rows_err)
+                return
+        }
+
+	err := rows.Err()
+	if err != nil {
+                c.AbortWithError(http.StatusBadRequest, err)
+                return
+        }
+}
+
 func main() {
 	v := router.Group("/apis/v1")
 	{
 		v.GET("/get_user", get_user_secure_func)
+		v.POST("/add_user", add_user_secure_func)
+		v.DELETE("/delete_user", delete_user_secure_func)
 	}
 	x509KP, x509KP_err := tls.LoadX509KeyPair("/etc/secret-volume/tls.crt", "/etc/secret-volume/tls.key")
 	if x509KP_err != nil {
